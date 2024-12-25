555a56cb12b1529c0a856b825b2721de9a28735c
munim <muhammadmunimoff330@gmail.com>
2024-12-25 15:18:34 +0500

    changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,8 +9,8 @@ export default {
     extend: {
       colors: {
        
-        // background: "#a49d95",
-        background: "#f4e7e3  ",
+        background: "#a49d95",
+        // background: "#f4e7e3  ",
         theme: "#720c2b",
       },
       borderWidth: {
