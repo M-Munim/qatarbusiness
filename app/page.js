b24e18974730a9e0c7567bd235afb559f08c3eeb
M-Munim <muhammadmunimoff330@gmail.com>
2025-01-01 10:59:18 +0500

    changes

--- app/page.js
+++ app/page.js
@@ -12,10 +12,9 @@ import React from 'react'
 const page = () => {
   return (
     <div className='px-16'>
-      <section className="w-full h-auto lg:h-[500px] mt-10 flex flex-wrap items-start justify-between px-4 ">
-        
-        <div className="w-full sm:w-4/12 lg:w-3/12 flex items-start justify-center flex-col px-5">
-          <h2 className="text-lg lg:text-3xl font-medium mb-1 text-center sm:text-left">
+      <section className="w-full h-[600px] flex flex-wrap items-start justify-between px-4 relative">   
+        <div className="w-full sm:w-4/12 lg:w-4/12 flex items-start justify-center flex-col px-5 relative z-10 top-1/3">
+          <h2 className="text-lg lg:text-4xl font-bold mb-1 text-center sm:text-left">
             Empowering Your Financial Success Globally
           </h2>
           <p className="text-xs mb-5 text-center sm:text-left popins leading-tight">
@@ -26,37 +25,17 @@ const page = () => {
             className="w-36 text-xl bg-white text-theme hover:bg-theme hover:text-white transition-all duration-200 mx-auto sm:mx-0"
           />
         </div>
-
       
-        <div className="w-full sm:w-6/12 lg:w-6/12 flex items-center justify-start my-5 sm:my-0">
-          <img src="/Screenshot 2024-12-25 at 3.17.58 PM.png" alt="logo" className="w-full" />
+        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2  w-full flex items-center justify-center">
+          <img src="/Screenshot 2024-12-25 at 3.17.58 PM.png" alt="logo" className="w-[1100px]" />
         </div>
-
         
-        <div className="w-full sm:w-4/12 lg:w-3/12 flex items-center justify-center ">
-         
-          <img src="/united-states.png" alt="" />
+        <div className="w-full sm:w-4/12 lg:w-3/12 flex items-center justify-center relative z-10">
+          <img src="/pngwing.com.png" alt="" className='w-[200px] h-[200px] mt-5' />
         </div>
       </section>
-      {/* <section className="w-full h-[500px] mt-10 flex items-start justify-center relative bg-cover bg-center px-4"
-        style={{ backgroundImage: "url('/Screenshot 2024-12-25 at 3.17.58 PM.png')" }}>
-        <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-        
-        <div className="absolute w-full sm:w-4/12 lg:w-3/12 flex flex-col items-start justify-center px-5 z-10 left-0 top-8">
-          <h2 className="text-lg lg:text-3xl font-medium mb-1  text-center sm:text-left">
-            Empowering Your Financial Success Globally
-          </h2>
-          <p className="text-xs mb-5 text-center sm:text-left popins leading-tight">
-            Lorem ipsum dolor sit amet voluptatum?
-          </p>
-          <Button
-            text="home"
-            className="w-36 text-xl bg-white text-theme hover:bg-theme hover:text-white transition-all duration-200 mx-auto sm:mx-0"
-          />
-        </div>
-      </section> */}
 
-      <section className="w-full -mt-10">
+      <section className="w-full">
         <Heading text="Our Expertise, Your Success" />
         <ServiceCard cards={cardsData} />
       </section>
--- components/Button.jsx
+++ components/Button.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
-const Button = ({ text, className }) => {
+const Button = ({ text, className, link = '#' }) => {
   return (
-    <>
-      {/* <button className='w-36 rounded-full h-10 bg-white text-theme capitalize font-semibold'>{text}</button> */}
-      <button className={`${className} capitalize font-semibold rounded-full h-11`} type="submit">{text}</button>
-    </>
-  )
-}
+    <Link href={link}>
+      <button className={`${className} capitalize font-semibold rounded-full h-11`} type="button">
+        {text}
+      </button>
+    </Link>
+  );
+};
 
-export default Button
+export default Button;
